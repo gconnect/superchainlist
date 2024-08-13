7e2c3be13d267982372db53f5f7317208c4eb01c
gconnect <agatevureglory@gmail.com>
2024-08-13 11:34:45 +0100

    Add encoding to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push('encoding', 'pino' /* add any other modules that might be causing the error */);
+    return config;
+  },
+};;
 
 export default nextConfig;
